af38fa210e2d93c8b9966d9037470c1132bc72db
Cigs-266 <wc266@kent.ac.uk>
2024-10-28 00:45:34 +0000

    3rd

--- ListManipulation.py
+++ ListManipulation.py
@@ -1,1 +1,14 @@
 ### Part Three -- your code goes here. 
+#First list
+numbers = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+#sorting
+numbers.sort()
+
+#removing "1"
+numbers = [num for num in numbers if num != 1]
+
+#extend list with 7, 8
+numbers.extend ([7, 8])
+
+print("Final List!", numbers)
